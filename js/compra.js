df03a3ac4b2353d2c879c5bd57147916426d287e
moradan <realmoradan@hotmail.com>
2023-05-13 22:52:12 -0300

    primer boceto de modelo de objeto

---
+++ js/compra.js
@@ -0,0 +1,10 @@
+// objeto formulario?
+
+//necesito event listeners para:
+//boton borrar
+//boton calcular
+//cambio de nombre para validar que no haya numeros
+//cambio de cantidad para validar que no haya letras
+//metodos para validar los datos
+//metodo para calcular el total
+//metodo para actualizar el campo de salida.

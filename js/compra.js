7f3e139e29ac5a2758adeae1af0578e1cf21ac7f
moradan <realmoradan@hotmail.com>
2023-05-15 17:34:30 -0300

    agregue verificacion de cantidad negativa

--- js/compra.js
+++ js/compra.js
@@ -8,7 +8,8 @@ const formulario = {
     campoCantidad: document.querySelector("#campoCantidad"),
     campoCategoria: document.querySelector("#campoCategoria"),
     campoTotal: document.querySelector("#campoTotal"),
-    mensajeAdvertencia: document.querySelector("#advertencia"),
+    mensajeAdvertenciaDatos: document.querySelector("#advertenciaDatos"),
+    mensajeAdvertenciaCantidad: document.querySelector("#advertenciaCantidad"),
 
     descuento: {
         estudiante: 0.8,
@@ -39,19 +40,27 @@ const formulario = {
 
     mostrarResumen: function() {
         const cantidad = this.campoCantidad.value;
-        const categoria = this.campoCategoria.value;
-        const costoTotal = this.valorTicket * cantidad * (1 - this.descuento[categoria]);
-        this.campoTotal.innerHTML = `$${costoTotal.toFixed(2)}`;
-
-        if (this.campoNombre.value == "" || this.campoApellido.value == "" || this.campoEmail.value == "") {
-            this.mensajeAdvertencia.classList.remove("d-none");
+        
+        if (cantidad < 0) {
+            this.mensajeAdvertenciaCantidad.classList.remove("d-none");
+            this.campoCantidad.value = "";
+            this.campoCantidad.focus();
         } else {
-            this.mensajeAdvertencia.classList.add("d-none");
-        }
+            this.mensajeAdvertenciaCantidad.classList.add("d-none");
+            const categoria = this.campoCategoria.value;
+            const costoTotal = this.valorTicket * cantidad * (1 - this.descuento[categoria]);
+            this.campoTotal.innerHTML = `$${costoTotal.toFixed(2)}`;
+    
+            if (this.campoNombre.value == "" || this.campoApellido.value == "" || this.campoEmail.value == "") {
+                this.mensajeAdvertenciaDatos.classList.remove("d-none");
+            } else {
+                this.mensajeAdvertenciaDatos.classList.add("d-none");
+            }
+        }        
     },
 
     limpiar: function () {
-        this.mensajeAdvertencia.classList.add("d-none");
+        this.mensajeAdvertenciaDatos.classList.add("d-none");
         this.campoTotal.innerHTML = "";
         this.campoNombre.focus();
     }

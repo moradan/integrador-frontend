2d361905a67b30900abf8a9876fedd452428ec69
moradan <realmoradan@hotmail.com>
2023-05-15 10:44:55 -0300

    validar datos; mensaje de advertencia;

--- js/compra.js
+++ js/compra.js
@@ -3,9 +3,12 @@ const formulario = {
     botonResumen: document.querySelector("#botonResumen"),
     botonReset: document.querySelector("#botonReset"),
     campoNombre: document.querySelector("#campoNombre"),
+    campoApellido: document.querySelector("#campoApellido"),
+    campoEmail: document.querySelector("#campoEmail"),
     campoCantidad: document.querySelector("#campoCantidad"),
     campoCategoria: document.querySelector("#campoCategoria"),
     campoTotal: document.querySelector("#campoTotal"),
+    mensajeAdvertencia: document.querySelector("#advertencia"),
 
     descuento: {
         estudiante: 0.8,
@@ -16,8 +19,9 @@ const formulario = {
     inicializar: function() {
         this.botonResumen.onclick = this.mostrarResumen.bind(this);
         this.botonReset.onclick = this.limpiar.bind(this);
-        window.onkeydown = this.evaluarTecla.bind(this);
         this.campoCategoria.onkeypress = this.evaluarTecla.bind(this);
+        window.onkeydown = this.evaluarTecla.bind(this);
+        this.limpiar();
     },
 
     evaluarTecla: function(evento) {
@@ -38,6 +42,12 @@ const formulario = {
         const categoria = this.campoCategoria.value;
         const costoTotal = this.valorTicket * cantidad * (1 - this.descuento[categoria]);
         this.campoTotal.innerHTML = `$${costoTotal.toFixed(2)}`;
+
+        if (this.campoNombre.value == "" || this.campoApellido.value == "" || this.campoEmail.value == "") {
+            this.mensajeAdvertencia.classList.remove("d-none");
+        } else {
+            this.mensajeAdvertencia.classList.add("d-none");
+        }
     },
 
     limpiar: function () {
